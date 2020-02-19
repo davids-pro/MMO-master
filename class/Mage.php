6dc65adb431c6a796900c02fedc12d28764eafb1
kamel-yhf <kamelyakhlef@hotmail.fr>
2020-02-19 11:12:34 +0100

    methode specifique sort

--- class/Mage.php
+++ class/Mage.php
@@ -1,1 +1,12 @@
 ﻿<?php
+
+class Mage extends Personnage{
+
+    private $mana;
+
+
+//METHODE SPECIFIQUE
+    public function sort(Mage $mage){
+        echo "je suis " . $mage->getNom() . " et je suis plein d'énergie ";
+    }
+}
