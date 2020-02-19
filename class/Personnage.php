3b0353fe29c191b9e858304ed108003b7d1d3140
davids-pro <54986025+davids-pro@users.noreply.github.com>
2020-02-19 10:16:13 +0100

    Personnage OK

---
+++ class/Personnage.php
@@ -0,0 +1,91 @@
+<?php
+class Personnage
+{
+  private $nom;
+  private $force;
+  private $sante;
+
+  public function __construct($nom, $force, $sante)
+  {
+    $this->setNom($nom);
+    $this->setForce($force);
+    $this->setSante($sante);
+  }
+
+
+  //############
+  //#  GETTER  #
+  //############
+
+  /**
+   * return: $nom
+   * type: string
+   */
+  public function getNom()
+  {
+    return $this->nom;
+  }
+
+  /**
+   * return: $force
+   * type: integer
+   */
+  public function getForce()
+  {
+    return $this->force;
+  }
+
+  /**
+   * return: $sante
+   * type: integer
+   */
+  public function getSante()
+  {
+    return $this->sante;
+  }
+
+  //##########
+  //# Setter #
+  //##########
+
+  /**
+   * set value : $nom
+   * type : string
+   */
+  public function setNom(string $nom)
+  {
+    $this->nom = $nom;
+  }
+
+  /**
+   * set value : $force
+   * type : integer
+   */
+  public function setForce(int $force)
+  {
+    $this->force = $force;
+  }
+
+  /**
+   * set value : $sante
+   * type : integer
+   */
+  public function setSante(int $sante)
+  {
+    $this->sante = $sante;
+  }
+
+  //####################
+  //# SPECIFIC METHODS #
+  //####################
+
+  function seDeplacer(Personnage $personnage)
+  {
+    echo 'Je suis' . $personnage->getNom() . 'et je me deplace';
+  }
+
+  function parler(Personnage $personnage)
+  {
+    echo 'Je suis' . $personnage->getNom() . 'et je parle';
+  }
+}
